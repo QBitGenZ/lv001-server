62ca7c573fcaf13b66a0a6bed9bd9cd07647381c
nntruyen027 <nntruyen027@gmail.com>
2024-03-22 17:58:42 +0700

    them get info

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user_management.views import AdminUserView, UserView, RegisterView, LoginView
+from user_management.views import AdminUserView, UserView, RegisterView, LoginView, GetInfoView
 
 urlpatterns = [
     path('admin-user', AdminUserView.as_view()),
     path('user', UserView.as_view()),
     path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view())
+    path('login', LoginView.as_view()),
+    path('info', GetInfoView.as_view()),
 ]
--- user_management/views.py
+++ user_management/views.py
@@ -137,3 +137,10 @@ class RegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GetInfoView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        serializer = UserSerializer(user, many=False)
+        return Response({'data': serializer.data}, status=status.HTTP_200_OK)
