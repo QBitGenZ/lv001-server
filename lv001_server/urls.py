a5a33f66624a46af5ef51bc3bf82abcda1cb7f7f
nntruyen027 <nntruyen027@gmail.com>
2024-03-23 00:47:29 +0700

    thêm hình ảnh

--- lv001_server/urls.py
+++ lv001_server/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 version = 'v1'
@@ -26,3 +27,5 @@ urlpatterns = [
     path(version + '/addresses/', include('address.urls')),
     path(version + '/orders/', include('order.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
