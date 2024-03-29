a207bc29244f0fe527c5b93f7843c5d20fb151a5
nntruyen027 <nntruyen027@gmail.com>
2024-03-30 01:45:46 +0700

    sửa gio

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from user_management.views import AdminUserView, UserView, RegisterView, LoginView, GetInfoView
+from user_management.views import AdminUserView, UserView, RegisterView, LoginView, GetInfoView, ChangeStatusView
 
 urlpatterns = [
     path('admin-user', AdminUserView.as_view()),
@@ -8,5 +8,5 @@ urlpatterns = [
     path('register', RegisterView.as_view()),
     path('login', LoginView.as_view()),
     path('info', GetInfoView.as_view()),
-    path('status/<str:username>', GetInfoView.as_view()),
+    path('status/<str:username>', ChangeStatusView.as_view()),
 ]
