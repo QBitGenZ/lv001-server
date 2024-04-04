6b2a7a6a79ec6e520d19574f1f3e51148b95a535
nntruyen027 <nntruyen027@gmail.com>
2024-04-04 23:35:46 +0700

    Thêm sửa order

--- order/urls.py
+++ order/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', OrderListView.as_view(), name='order-list'),
     path('<uuid:pk>/', OrderListPkView.as_view(), name='order-detail'),
     path('myorders/', MyOrderListView.as_view(), name='my-order'),
-    path('<uuid:order>/items/', MyOrderListView.as_view(), name='create-item'),
+    path('items/<uuid:order>/', MyOrderListView.as_view(), name='create-item'),
     path('items/<uuid:pk>', OrderDetailView.as_view(), name='delete-item'),
 ]
