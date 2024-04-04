6554b0fe9e35f5c8ed57cee0c3c2dd0c83312052
nntruyen027 <nntruyen027@gmail.com>
2024-04-04 23:36:28 +0700

    Thêm sửa order

--- order/urls.py
+++ order/urls.py
@@ -3,8 +3,8 @@ from order.views import OrderListView, OrderListPkView, MyOrderListView, OrderDe
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='order-list'),
-    path('<uuid:pk>/', OrderListPkView.as_view(), name='order-detail'),
     path('myorders/', MyOrderListView.as_view(), name='my-order'),
     path('items/<uuid:order>/', MyOrderListView.as_view(), name='create-item'),
     path('items/<uuid:pk>', OrderDetailView.as_view(), name='delete-item'),
+    path('<uuid:pk>/', OrderListPkView.as_view(), name='order-detail'),
 ]
