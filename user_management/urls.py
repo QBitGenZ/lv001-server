c3da68813efebc33f500e14dc847704f5ab9fffe
nntruyen027 <nntruyen027@gmail.com>
2024-04-15 23:11:17 +0700

    Thm thay đổi mật khẩu

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from user_management.views import AdminUserView, UserView, RegisterView, LoginView, GetInfoView, ChangeStatusView
+from user_management.views import AdminUserView, UserView, RegisterView, LoginView, GetInfoView, ChangeStatusView, ChangePasswordView
 
 urlpatterns = [
     path('admin-user', AdminUserView.as_view()),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login', LoginView.as_view()),
     path('info', GetInfoView.as_view()),
     path('status/<str:username>', ChangeStatusView.as_view()),
+    path('change-password', ChangePasswordView.as_view())
 ]
