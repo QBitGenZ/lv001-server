7637d98562994b3a90a17f24add4b98cad149966
nntruyen027 <nntruyen027@gmail.com>
2024-04-24 01:50:01 +0700

    sửa xíu nữa

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from .views import ProductTypeView, ProductView, ProductPkView,ProductByUser,SoldProductView,TotalRevenueByUserAPIView, SearchProductView
+from .views import ProductTypeView, ProductView, ProductPkView,ProductByUser,SoldProductView,TotalRevenueByUserAPIView, SearchProductView, SearchProductByUserView
 
 urlpatterns = [
     path('types/', ProductTypeView.as_view(), name='product_types'),
     path('types/<uuid:pk>/', ProductTypeView.as_view(), name='product_types'),
     path('', ProductView.as_view(), name='products'),
     path('search/', SearchProductView.as_view(), name='search'),
-    path('myproducts/search/', SearchProductView.as_view(), name='search'),
+    path('myproducts/search/', SearchProductByUserView.as_view(), name='search'),
     path('<uuid:pk>/', ProductPkView.as_view(), name='products'),
     path('images/', include('resource.urls')),
     path('feedbacks/', include('feedback.urls')),
