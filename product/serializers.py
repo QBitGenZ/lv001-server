aa3d5ee0ecc43dbe19250f39f2fc7ccdd9b2c6ef
nntruyen027 <nntruyen027@gmail.com>
2024-04-25 14:52:52 +0700

    thểm hiển thị cái sold

--- product/serializers.py
+++ product/serializers.py
@@ -18,7 +18,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'quantity', 'price', 'product_type', 'product_detail', 'product_image',
+        fields = ['id', 'name', 'quantity', 'sold', 'price', 'product_type', 'product_detail', 'product_image',
                   'created_at', 'user', 'product_feedback', 'status', 'degree', 'created_at', 'size', 'description', 'gender']
 
 
