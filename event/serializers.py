dd91b17ea46826e9b96875d226babacd0bbfebb8
nntruyen027 <nntruyen027@gmail.com>
2024-05-01 14:50:15 +0700

    chỉnh sửa tí

--- event/serializers.py
+++ event/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from event.models import Event, DonantionProduct
-
+from product.serializers import ProductDetailSerializer
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,6 +14,7 @@ class EventSerializer(serializers.ModelSerializer):
         }
 
 class DonantionProductSerializer(serializers.ModelSerializer):
+    product = ProductDetailSerializer()
     class Meta:
         model = DonantionProduct
         fields = '__all__'
